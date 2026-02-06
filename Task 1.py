22af76eb4faf12a8e6cb0cd1632eeab61248c464
anuragverma001 <anurag01verma01@gmail.com>
2026-02-06 10:03:46 +0530

    Task 1

---
+++ Task 1.py
@@ -0,0 +1,13 @@
+#Task 1
+#Perform Basic Mathematical Operations
+#Addition
+#Substraction
+#Multiplication
+#Division
+print("Basic Mathematical Operations")
+N1 = float(input("Enter first number : "))
+N2 = float(input("Enter Second Number : "))
+print("Addition:", (N1 + N2), "\nSubstraction:", (N1-N2), "\nMultiplication: ", (N1+N2), "\nDivision: ", (N1/N2))
+
+
+
